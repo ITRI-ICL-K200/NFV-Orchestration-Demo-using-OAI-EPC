ddd2bf5721cb87d29810721a980a305785ba450f
YaoJenTang <35796356+YaoJenTang@users.noreply.github.com>
2018-01-29 11:06:31 +0800

    Update script path in hss_test.sh
    /opt/openbaton/scripts/ -> /opt/openbaton/scripts/OpenBaton/scripts/

--- OpenBaton/hss_test.sh
+++ OpenBaton/hss_test.sh
@@ -8,9 +8,9 @@ sed -i "s/$ORGHNAME/hss/" /etc/hostname
 hostname hss
 
 #start HSS
-sudo cp /opt/openbaton/scripts/hss.conf /usr/local/etc/oai
-sudo cp /opt/openbaton/scripts/hss_fd.conf /usr/local/etc/oai/freeDiameter
-sudo cp /opt/openbaton/scripts/acl.conf /usr/local/etc/oai/freeDiameter
-sudo cp /opt/openbaton/scripts/HSS.service /etc/systemd/system
-sudo sh /opt/openbaton/scripts/create_db.sh
+sudo cp /opt/openbaton/scripts/OpenBaton/scripts/hss.conf /usr/local/etc/oai
+sudo cp /opt/openbaton/scripts/OpenBaton/scripts/hss_fd.conf /usr/local/etc/oai/freeDiameter
+sudo cp /opt/openbaton/scripts/OpenBaton/scripts/acl.conf /usr/local/etc/oai/freeDiameter
+sudo cp /opt/openbaton/scripts/OpenBaton/scripts/HSS.service /etc/systemd/system
+sudo sh /opt/openbaton/scripts/OpenBaton/scripts/create_db.sh
 sudo service HSS start
