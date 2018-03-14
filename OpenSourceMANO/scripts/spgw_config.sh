a09fb8feb9422730efe5697359f570dce3f2574c
OaiTestBase <34823716+OaiTestBase@users.noreply.github.com>
2018-03-14 16:42:55 +0800

    Update spgw_config.sh

--- OpenSourceMANO/scripts/spgw_config.sh
+++ OpenSourceMANO/scripts/spgw_config.sh
@@ -15,15 +15,15 @@ S1_UInterface=`ifconfig | grep -B1 "inet addr:$1" | awk '$1!="inet" && $1!="--" 
 S11Interface=`ifconfig | grep -B1 "inet addr:$2" | awk '$1!="inet" && $1!="--" {print $1}'`
 
 #config files
-sudo sed -i "s/__SPGW_SGi_INTERFACE__/${SGiInterface}/" ~/testInfo/test/spgw.conf
-sudo sed -i "s/__SPGW_S1U_IP__/$1/" ~/testInfo/test/spgw.conf
-sudo sed -i "s/__SPGW_S1U_INTERFACE__/${S1_UInterface}/" ~/testInfo/test/spgw.conf
-sudo sed -i "s/__SPGW_S11_IP__/$2/" ~/testInfo/test/spgw.conf
-sudo sed -i "s/__SPGW_S11_INTERFACE__/${S11Interface}/" ~/testInfo/test/spgw.conf
+sudo sed -i "s/__SPGW_SGi_INTERFACE__/${SGiInterface}/" ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf
+sudo sed -i "s/__SPGW_S1U_IP__/$1/" ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf
+sudo sed -i "s/__SPGW_S1U_INTERFACE__/${S1_UInterface}/" ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf
+sudo sed -i "s/__SPGW_S11_IP__/$2/" ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf
+sudo sed -i "s/__SPGW_S11_INTERFACE__/${S11Interface}/" ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf
 
 #copy files
-sudo cp ~/testInfo/test/spgw.conf /usr/local/etc/oai
-sudo cp ~/testInfo/test/SPGW.service /etc/systemd/system
+sudo cp ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/spgw.conf /usr/local/etc/oai
+sudo cp ~/NFV-Orchestration-Demo-using-OAI-EPC/OpenSourceMANO/scripts/SPGW.service /etc/systemd/system
 
 #run spgw
 sudo service SPGW start
